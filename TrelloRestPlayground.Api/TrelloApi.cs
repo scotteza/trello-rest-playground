c2e575cfa9df17d7a0e7dde72e9ff106e3d998e5
Scott Edwards <scotteza@gmail.com>
2022-07-15 21:52:51 +0100

    Refactor

--- TrelloRestPlayground.Api/TrelloApi.cs
+++ TrelloRestPlayground.Api/TrelloApi.cs
@@ -7,18 +7,33 @@ public class TrelloApi
 {
     public async Task<IEnumerable<TrelloBoard>?> FetchBoards()
     {
+        var url = GetUrl();
+        var responseBody = await GetHttpResponse(url);
+        var result = DeserializeHttpResponse(responseBody);
+        return result;
+    }
+
+    private string GetUrl()
+    {
         var token = new TrelloAuthorizer().GetToken();
         var key = new TrelloAuthorizer().GetKey();
         var url = $"https://api.trello.com/1/members/me/boards?key={key}&token={token}";
+        return url;
+    }
 
+    private async Task<string> GetHttpResponse(string url)
+    {
         var httpClient = new HttpClient();
         var response = await httpClient.GetAsync(url);
         response.EnsureSuccessStatusCode();
         var responseBody = await response.Content.ReadAsStringAsync();
+        return responseBody;
+    }
 
+    private IEnumerable<TrelloBoard>? DeserializeHttpResponse(string responseBody)
+    {
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         var result = JsonSerializer.Deserialize<TrelloBoard[]>(responseBody, options);
-
         return result;
     }
 }
